2f6b0593c57304ae0b2a6c2741dc47f0cef8214c
Sarah Hart <sarah.hart.nc@gmail.com>
2022-10-24 02:45:21 -0400

    first commit

---
+++ main.java
@@ -0,0 +1,7 @@
+// Name Entered into value 
+// On click "Welcome to my shop" headers 
+// "Thank you" dynamic name as the body
+
+// No first and/or last name on submit 
+// First or last name non-text data 
+// When no radio button is selected on submit
