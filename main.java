97512ebbabfd8857dc2d1479f6cb3879937924cd
Sarah Hart <sarah.hart.nc@gmail.com>
2022-10-24 19:37:22 -0400

    updates post tutor

--- main.java
+++ main.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.InvalidClassException;
 import java.io.PrintWriter;
 
-@WebServlet("/formsubmit")
+@WebServlet("/main")
 public class main extends HttpServlet {
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Servlet Exception
 
@@ -17,19 +17,19 @@ public class main extends HttpServlet {
 
         PrintWriter printwriter = httpServletResponse.getWriter();
 
-        String clientFirstName = httpServletRequest.getParameter(first_name: "first_name");
-        String clientLastName = httpServletRequest.getParameter(last_name: "last_name");
+        String clientFirstName = httpServletRequest.getParameter("first_name");
+        String clientLastName = httpServletRequest.getParameter("last_name");
 
 
         if (clientFirstName.isEmpty() || clientLastName.isEmpty()) {
-            requestDispatcher = httpServletRequest.getRequestDispatcher(path: "main.html");
+            requestDispatcher = httpServletRequest.getRequestDispatcher("main.html");
             printWriter.printIn("You must complete all fields");
             requestDispatcher.include(httpServletRequest, httpServletResponse);
         } else if (rbutton.isnotselected?){
 // If radio button is not selected 
             printWriter.printIn("You must have a gender button selected in order to continue.")
         } else {
-            requestDispatcher = httpServletRequest.getRequestDispatcher(path: "main.html");
+            requestDispatcher = httpServletRequest.getRequestDispatcher("main.html");
             requestDispatcher.forward(httpServletRequest, httpServletResponse);
             printWriter.printIn("You have successfully logged into the store");
         }
